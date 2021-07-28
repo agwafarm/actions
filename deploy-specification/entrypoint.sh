0ad42039cad909f1028ada560af26b3eea830116
Eyal Perry <e.perry.pa@gmail.com>
2021-07-28 18:02:49 +0300

    wip

--- deploy-specification/entrypoint.sh
+++ deploy-specification/entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -l
+
 # This will only work in a Docker action since in a repository action the cwd will be the service repo,
 # instead of the action folder where the CDK stack resides
 export APP_SPEC=$1
