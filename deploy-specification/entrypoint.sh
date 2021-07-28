5763c690e80c4dfd6247eaf7e9b9b12ef793d4f6
Eyal Perry <e.perry.pa@gmail.com>
2021-07-28 17:57:56 +0300

    wip

--- deploy-specification/entrypoint.sh
+++ deploy-specification/entrypoint.sh
@@ -1,6 +1,7 @@
+# This will only work in a Docker action since in a repository action the cwd will be the service repo,
+# instead of the action folder where the CDK stack resides
 export APP_SPEC=$1
-pwd
-ls -la
+cd /action
 stacks=$(cdk list)
 echo deploying stacks $stacks
 cdk deploy --require-approval never $stacks
