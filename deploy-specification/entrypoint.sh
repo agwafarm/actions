d1d2e3c7036da0ee225f9558d6f7165f84269145
Eyal Perry <e.perry.pa@gmail.com>
2021-07-28 20:19:28 +0300

    wip

--- deploy-specification/entrypoint.sh
+++ deploy-specification/entrypoint.sh
@@ -3,12 +3,6 @@
 # This will only work in a Docker action since in a repository action the cwd will be the service repo,
 # instead of the action folder where the CDK stack resides
 export APP_SPEC=$1
-export BASE_FOLDER=$(pwd)
-
-echo base folder $BASE_FOLDER
-ls -la $BASE_FOLDER
-
-ls -la ./specs
 
 cd /action
 stacks=$(cdk list)
