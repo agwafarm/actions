4854168dc62291f0d790dc5facceeedf6da9da43
Eyal Perry <e.perry.pa@gmail.com>
2021-11-09 17:11:49 +0200

    bugfix

--- fe-depargs/index.js
+++ fe-depargs/index.js
@@ -33,7 +33,7 @@ async function run() {
 
     const env = computeEnv();
     const bucketPrefix = "/" + github.context.sha;
-    const bucketName = `${env}-${companyName}-${app}-web`;
+    const bucketName = `${env}-${companyName}-${app}-webapp`;
 
     console.log(`will deploy to environment: ${env}`);
     console.log("bucket name", bucketName);
