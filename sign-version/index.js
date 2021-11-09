e270592b49310313c0fd92b7f34c487d00dc822f
Eyal Perry <e.perry.pa@gmail.com>
2021-11-09 12:38:04 +0200

    bugfix

--- sign-version/index.js
+++ sign-version/index.js
@@ -106,7 +106,7 @@ async function createVersion({ name, spec, timestamp, datetime, author }) {
         { Key: "timestamp", Value: `${timestamp}` },
         { Key: "datetime", Value: `${datetime.toISOString()}` },
         { Key: "versionName", Value: `${name}` },
-        { Key: "overrides", Value: Object.keys(overrides).join(":") },
+        { Key: "overrides", Value: Object.keys(overrides).join(":") || "none" },
       ],
     })
   );
