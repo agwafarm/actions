22c03a4363f6f5b78f99ae57ed01e5e8d6195d8f
Eyal Perry <e.perry.pa@gmail.com>
2021-11-16 16:34:03 +0200

    wip

--- deploy-frontend/entrypoint.sh
+++ deploy-frontend/entrypoint.sh
@@ -109,8 +109,9 @@ if [ "$s3_retainment" = "standard" ]; then
    param_name=/infra/rc-version/$service_name
    param_value="{\"version\":\"$rc_version\", \"type\":\"frontend\"}"
 
-   echo ssm param name $param_name
-   echo ssm param value $param_value
+   echo "Updating RC pointer"
+   echo "RC pointer name" $param_name
+   echo "RC pointer value" $param_value
 
    aws ssm put-parameter --type String --overwrite --name $param_name --value $param_value
 fi
