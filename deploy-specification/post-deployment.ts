eb5d61b406a5c293ad1f5b4caad85e03d0b6ab77
Eyal Perry <e.perry.pa@gmail.com>
2021-11-17 17:56:09 +0200

    bugfix

--- deploy-specification/post-deployment.ts
+++ deploy-specification/post-deployment.ts
@@ -2,13 +2,13 @@ import { SSMClient, PutParameterCommand } from "@aws-sdk/client-ssm";
 
 import { spawn } from "child_process";
 
-console.log("running post-deployment script");
-
 const ssmClient = new SSMClient({ region: "us-west-2" });
 const mode = process.env["APP_MODE"] || "service";
 const { env, version, frontends } = JSON.parse(
   process.env["APP_SPEC"] as string
 );
+
+console.log(`running post-deployment script for version: ${version}`);
 
 async function updateEnvPointer() {
   if (mode !== "env") {
@@ -37,14 +37,20 @@ function syncBuckets(sourcePrefix: string, targetBucket: string) {
       `s3://${targetBucket}`,
     ];
 
-    const child = spawn("aws", syncArgs, { env: process.env });
-    child.on("exit", function (code, signal) {
+    const child = spawn("aws", syncArgs, {
+      env: process.env,
+      cwd: process.cwd(),
+    });
+    child.on("exit", function (code: any, signal: any) {
       if (code || signal) {
         console.log(
           `failed to sync source: ${sourcePrefix} with target bucket ${targetBucket}`
         );
         reject(code || signal);
       } else {
+        console.log(
+          `successfully synced prefix: ${sourcePrefix} with target bucket: ${targetBucket}`
+        );
         resolve("");
       }
     });
@@ -52,7 +58,14 @@ function syncBuckets(sourcePrefix: string, targetBucket: string) {
 }
 
 async function updateS3Artifacts() {
+  if (mode !== "env") {
+    return;
+  }
+
+  console.log(`syncing s3 buckets for environment ${env}`);
+
   const promises: Promise<any>[] = [];
+
   for (const frontend of frontends) {
     const frontendBucket = frontend.parameters.BucketName;
     const sourcePrefix = `standard/${frontend.name}/${frontend.version}/web/${env}`;
@@ -61,6 +74,7 @@ async function updateS3Artifacts() {
     promises.push(syncBucketsPromise);
   }
 
+  console.log(`waiting for all sync commands to finish`);
   await Promise.all(promises);
 }
 
