0e623d0ae1b7f40e5d10c1a8753dbc6e51ab2794
Eyal Perry <e.perry.pa@gmail.com>
2022-01-20 11:41:39 +0200

    bugfix

--- deploy-specification/post_deployment.py
+++ deploy-specification/post_deployment.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+from subprocess import Popen
 mode = os.environ.get("APP_MODE", "service");
 spec = json.loads(os.environ['APP_SPEC'])
 env = spec.get('env')
@@ -10,5 +10,8 @@ service_names = [service['serviceName'] for service in services]
 if mode == 'env' or 'greengrass-parent' in service_names:
     print(f'deploying greengrass definitions to devices in environment: {env}')
     os.environ['AGWA_SERVICE_LIBRARY_TAG'] = 'latest'
-    os.system(f'py-prepare.sh')
-    os.system(f'python3 gg_deploy.py -e {env}')
+    prepare = Popen(["py-prepare.sh"])
+    prepare.wait()
+    
+    deploy = Popen(["python3", "gg_deploy.py", "-e", env])
+    deploy.wait()
