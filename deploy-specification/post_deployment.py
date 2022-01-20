13582790635ba41d5a6f45568e5bd22165427b77
Eyal Perry <e.perry.pa@gmail.com>
2022-01-20 10:36:41 +0200

    deploy-specification with gg deploy

---
+++ deploy-specification/post_deployment.py
@@ -0,0 +1,13 @@
+from ensurepip import version
+import os
+import json
+
+mode = os.environ.get("APP_MODE", "service");
+spec = json.loads(os.environ['APP_SPEC'])
+env = spec.get('env')
+services = spec.get('services')
+service_names = [service['serviceName'] for service in services]
+
+if mode == 'env' or 'greengrass-parent' in service_names:
+    print(f'deploying greengrass definitions to devices in environment: {env}')
+    os.system(f'python3 gg_deploy.py -e {env}')
