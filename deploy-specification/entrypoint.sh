af1ec6675d35b44085817970414699778ea2409d
Eyal Perry <e.perry.pa@gmail.com>
2022-01-20 15:26:59 +0200

    wip

--- deploy-specification/entrypoint.sh
+++ deploy-specification/entrypoint.sh
@@ -7,7 +7,5 @@ set -o pipefail
 export APP_SPEC=$1
 export APP_MODE=$2
 
-cd /action
-
-export APP_STACKS=$(npm run describe)
-npm run deploy -- deploy --require-approval never $APP_STACKS && npx ts-node --prefer-ts-exts ./post-deployment.ts && python3 post_deployment.py
+export APP_STACKS=$(cdk list)
+cdk deploy --require-approval never $APP_STACKS && npx ts-node --prefer-ts-exts ./post-deployment.ts && python3 post_deployment.py
