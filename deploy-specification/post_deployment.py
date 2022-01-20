1fb9de04aea4c982b5b27d96c6f1752cf6042872
Eyal Perry <e.perry.pa@gmail.com>
2022-01-20 11:45:32 +0200

    wip

--- deploy-specification/post_deployment.py
+++ deploy-specification/post_deployment.py
@@ -10,7 +10,7 @@ service_names = [service['serviceName'] for service in services]
 if mode == 'env' or 'greengrass-parent' in service_names:
     print(f'deploying greengrass definitions to devices in environment: {env}')
     os.environ['AGWA_SERVICE_LIBRARY_TAG'] = 'latest'
-    prepare = Popen(["py-prepare.sh"])
+    prepare = Popen(["./py-prepare.sh"])
     prepare.wait()
     
     deploy = Popen(["python3", "gg_deploy.py", "-e", env])
