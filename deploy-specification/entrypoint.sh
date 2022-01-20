2e4e0f03149a2d5bfffe56e8367b0fede2db43d3
Eyal Perry <e.perry.pa@gmail.com>
2022-01-20 11:18:12 +0200

    wip

--- deploy-specification/entrypoint.sh
+++ deploy-specification/entrypoint.sh
@@ -10,4 +10,4 @@ export APP_MODE=$2
 cd /action
 
 export APP_STACKS=$(cdk list)
-cdk deploy --require-approval never $APP_STACKS && npx ts-node --prefer-ts-exts ./post-deployment.ts
+cdk deploy --require-approval never $APP_STACKS && npx ts-node --prefer-ts-exts ./post-deployment.ts && python3 post_deployment.py
