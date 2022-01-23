2952df69545263375b7ae8c14b2f4f3f94f8c6f0
Eyal Perry <e.perry.pa@gmail.com>
2022-01-23 10:58:55 +0200

    do not support dev env auto deployment

--- deploy-specification/post_deployment.py
+++ deploy-specification/post_deployment.py
@@ -6,10 +6,10 @@ from subprocess import Popen
 mode = os.environ.get("APP_MODE", "service");
 spec = json.loads(os.environ['APP_SPEC'])
 env = spec.get('env')
-services = spec.get('services')
-service_names = [service['serviceName'] for service in services]
 
-if mode == 'env' or 'greengrass-parent' in service_names:
+# we do not deploy to dev branches automatically since mysql lists all dev devices in one database.
+# this means automatically deploying breaks when all devices are deployed to a specific env
+if mode == 'env' or env == 'ci':
     print(f'deploying greengrass definitions to devices in environment: {env}')
     os.environ['AGWA_SERVICE_LIBRARY_TAG'] = 'latest'
     prepare = Popen(["./py-prepare.sh"])
