83473505ecbdf06e9411ef074104ea141e307437
Niv Stolarski <nivsto@gmail.com>
2022-12-03 23:52:57 +0200

    Enable skipping GG deployment

--- deploy-specification/entrypoint.sh
+++ deploy-specification/entrypoint.sh
@@ -8,6 +8,7 @@ cd /action
 
 export APP_SPEC=$1
 export APP_MODE=$2
+export APP_EDGE_DEPLOYMENT=$3
 
 export APP_STACKS=$(cdk list)
 cdk deploy --require-approval never $APP_STACKS && npx ts-node --prefer-ts-exts ./post-deployment.ts && python3 post_deployment.py
--- deploy-specification/post_deployment.py
+++ deploy-specification/post_deployment.py
@@ -3,13 +3,14 @@ import json
 import sys
 
 from subprocess import Popen
-mode = os.environ.get("APP_MODE", "service");
+mode = os.environ.get("APP_MODE", "service")
 spec = json.loads(os.environ['APP_SPEC'])
+edge_deployment = os.environ.get("APP_EDGE_DEPLOYMENT", "deploy")
 env = spec.get('env')
 services = spec.get('services')
 service_names = [service['serviceName'] for service in services]
 
-if mode == 'env' or ('greengrass-parent' in service_names and env == 'ci'):
+if mode == 'env' or ('greengrass-parent' in service_names and env == 'ci') and edge_deployment == 'deploy':
     print(f'deploying greengrass definitions to devices in environment: {env}')
     os.environ['AGWA_SERVICE_LIBRARY_TAG'] = 'latest'
     prepare = Popen(["./py-prepare.sh"])
