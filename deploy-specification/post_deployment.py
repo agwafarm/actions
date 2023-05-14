26e7ef4e87a43f1d3f0f5f5a49c5c7aa20186c7b
Niv Stolarski <nivsto@gmail.com>
2023-05-14 11:03:25 +0300

    Changed the way gg devices are being deployed

--- deploy-specification/post_deployment.py
+++ deploy-specification/post_deployment.py
@@ -9,16 +9,14 @@ edge_deployment = os.environ.get("APP_EDGE_DEPLOYMENT", "deploy")
 env = spec.get('env')
 services = spec.get('services')
 service_names = [service['serviceName'] for service in services]
+version = spec.get('version')
 
 if (mode == 'env' or ('greengrass-parent' in service_names and env == 'ci')) and edge_deployment == 'deploy':
     print(f'deploying greengrass definitions to devices in environment: {env}')
-    os.environ['AGWA_SERVICE_LIBRARY_TAG'] = 'latest'
-    prepare = Popen(["./py-prepare.sh"])
-    prepare.wait()
-    if prepare.poll():
-        sys.exit(1)
-    
-    deploy = Popen(["python3", "gg_deploy.py", "-e", env])
-    deploy.wait()
-    if deploy.poll():
-        sys.exit(1)
+    payload = {"version_tag": version}
+    response = client.invoke(
+        FunctionName=f'{env}_devices_version_upgrader_lambda',
+        InvocationType='RequestResponse',
+        LogType='None',
+        Payload=json.dumps(payload)
+    )
