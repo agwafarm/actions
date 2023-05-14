76569349bebb8efe00ab8104f68550e175c1f497
Niv Stolarski <nivsto@gmail.com>
2023-05-14 19:04:02 +0300

    lambda client bugfix

--- deploy-specification/post_deployment.py
+++ deploy-specification/post_deployment.py
@@ -10,11 +10,13 @@ env = spec.get('env')
 services = spec.get('services')
 service_names = [service['serviceName'] for service in services]
 version = spec.get('version')
+lambda_client = boto3.client('lambda')
 
-if (mode == 'env' or ('greengrass-parent' in service_names and env == 'ci')) and edge_deployment == 'deploy':
+if (mode == 'env' or ('greengrass-parent' in service_names and env == 'ci')) \
+        and edge_deployment in ['deploy_tonight', 'deploy_now']:
     print(f'deploying greengrass definitions to devices in environment: {env}')
     payload = {"version_tag": version}
-    response = client.invoke(
+    response = lambda_client.invoke(
         FunctionName=f'{env}_devices_version_upgrader_lambda',
         InvocationType='RequestResponse',
         LogType='None',
