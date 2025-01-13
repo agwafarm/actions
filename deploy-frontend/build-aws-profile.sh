35e8070b7c803155933600c15d2f8622bc2ff9f9
Ofri Ivzori <ofri84@gmail.com>
2025-01-13 14:47:59 +0200

    use aws profile

--- deploy-frontend/build-aws-profile.sh
+++ deploy-frontend/build-aws-profile.sh
@@ -5,21 +5,41 @@ AWS_CONFIG_FILE="$HOME/.aws/config"
 
 mkdir -p "$HOME/.aws"
 
-echo "[$AWS_PROD_PROFILE]" >> $AWS_CREDENTIALS_FILE
-echo "aws_access_key_id = $PROD_AWS_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
-echo "aws_secret_access_key = $PROD_AWS_SECRET_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
-echo "" >> $AWS_CREDENTIALS_FILE
-echo "[$AWS_DEV_PROFILE]" >> $AWS_CREDENTIALS_FILE
-echo "aws_access_key_id = $DEV_AWS_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
-echo "aws_secret_access_key = $DEV_AWS_SECRET_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
+cat << EOF > $AWS_CREDENTIALS_FILE
+[$AWS_PROD_PROFILE]
+aws_access_key_id = $PROD_AWS_ACCESS_KEY
+aws_secret_access_key = $PROD_AWS_SECRET_ACCESS_KEY
 
-echo "[$AWS_PROD_PROFILE]" >> $AWS_CONFIG_FILE
-echo "region = $AWS_REGION" >> $AWS_CONFIG_FILE
-echo "output = json" >> $AWS_CONFIG_FILE
-echo ""
-echo "[profile $AWS_DEV_PROFILE]" >> $AWS_CONFIG_FILE
-echo "region = $DEV_AWS_REGION" >> $AWS_CONFIG_FILE
-echo "output = json" >> $AWS_CONFIG_FILE
+[$AWS_DEV_PROFILE]
+aws_access_key_id = $DEV_AWS_ACCESS_KEY
+aws_secret_access_key = $DEV_AWS_SECRET_ACCESS_KEY
+EOF
+
+cat << EOF > $AWS_CONFIG_FILE
+[$AWS_PROD_PROFILE]
+region = $AWS_REGION
+output = json
+
+[profile $AWS_DEV_PROFILE]
+region = $DEV_AWS_REGION
+output = json
+EOF
+
+# echo "[$AWS_PROD_PROFILE]" >> $AWS_CREDENTIALS_FILE
+# echo "aws_access_key_id = $PROD_AWS_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
+# echo "aws_secret_access_key = $PROD_AWS_SECRET_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
+# echo "" >> $AWS_CREDENTIALS_FILE
+# echo "[$AWS_DEV_PROFILE]" >> $AWS_CREDENTIALS_FILE
+# echo "aws_access_key_id = $DEV_AWS_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
+# echo "aws_secret_access_key = $DEV_AWS_SECRET_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
+
+# echo "[$AWS_PROD_PROFILE]" >> $AWS_CONFIG_FILE
+# echo "region = $AWS_REGION" >> $AWS_CONFIG_FILE
+# echo "output = json" >> $AWS_CONFIG_FILE
+# echo ""
+# echo "[profile $AWS_DEV_PROFILE]" >> $AWS_CONFIG_FILE
+# echo "region = $DEV_AWS_REGION" >> $AWS_CONFIG_FILE
+# echo "output = json" >> $AWS_CONFIG_FILE
 
 cat $AWS_CREDENTIALS_FILE
 cat $AWS_CONFIG_FILE
