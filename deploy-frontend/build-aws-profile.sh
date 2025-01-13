28ef59596b596b39360d7e8cd1b0b7e3e6f030bf
Ofri Ivzori <ofri84@gmail.com>
2025-01-13 15:11:55 +0200

    use aws profile

--- deploy-frontend/build-aws-profile.sh
+++ deploy-frontend/build-aws-profile.sh
@@ -24,23 +24,3 @@ output = json
 region = $DEV_AWS_REGION
 output = json
 EOF
-
-# echo "[$AWS_PROD_PROFILE]" >> $AWS_CREDENTIALS_FILE
-# echo "aws_access_key_id = $PROD_AWS_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
-# echo "aws_secret_access_key = $PROD_AWS_SECRET_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
-# echo "" >> $AWS_CREDENTIALS_FILE
-# echo "[$AWS_DEV_PROFILE]" >> $AWS_CREDENTIALS_FILE
-# echo "aws_access_key_id = $DEV_AWS_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
-# echo "aws_secret_access_key = $DEV_AWS_SECRET_ACCESS_KEY" >> $AWS_CREDENTIALS_FILE
-
-# echo "[$AWS_PROD_PROFILE]" >> $AWS_CONFIG_FILE
-# echo "region = $AWS_REGION" >> $AWS_CONFIG_FILE
-# echo "output = json" >> $AWS_CONFIG_FILE
-# echo ""
-# echo "[profile $AWS_DEV_PROFILE]" >> $AWS_CONFIG_FILE
-# echo "region = $DEV_AWS_REGION" >> $AWS_CONFIG_FILE
-# echo "output = json" >> $AWS_CONFIG_FILE
-
-cat $AWS_CREDENTIALS_FILE
-cat $AWS_CONFIG_FILE
-aws sts get-caller-identity
