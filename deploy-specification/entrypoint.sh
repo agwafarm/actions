062f455c13548867030317a48c2a4ba97cd598f2
Ofri Ivzori <ofri84@gmail.com>
2025-07-22 15:39:00 +0300

    fix deploy spec

--- deploy-specification/entrypoint.sh
+++ deploy-specification/entrypoint.sh
@@ -11,4 +11,5 @@ export APP_MODE=$2
 export APP_EDGE_DEPLOYMENT=$3
 
 export APP_STACKS=$(cdk list)
+npx cdk bootstrap --force
 cdk deploy --require-approval never $APP_STACKS && npx ts-node --prefer-ts-exts ./post-deployment.ts && python3 post_deployment.py
