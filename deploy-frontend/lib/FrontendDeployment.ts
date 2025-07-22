c98219a3007a22448d71966b19e48e9e4fea461e
Ofri Ivzori <ofri84@gmail.com>
2025-07-22 10:53:42 +0300

    fix cloudfront dist and dotenv warns

--- deploy-frontend/lib/FrontendDeployment.ts
+++ deploy-frontend/lib/FrontendDeployment.ts
@@ -5,7 +5,9 @@ import {
   aws_ssm as ssm,
   aws_iam as iam,
   aws_cloudfront as cloudfront,
+  aws_cloudfront_origins as origins,
   aws_route53 as route53,
+  aws_certificatemanager as acm,
 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
@@ -69,49 +71,38 @@ export class FrontendDeployment extends BaseStack {
       );
     }
 
-    const distribution = new cloudfront.CloudFrontWebDistribution(
+    const distribution = new cloudfront.Distribution(
       this,
       "CloudfrontWebDistribution",
       {
         defaultRootObject: indexPath,
-        errorConfigurations: [
+        defaultBehavior: {
+          origin: new origins.S3Origin(websiteBucket),
+          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // SPA, no HTML caching
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        errorResponses: [
           // Give SPA control over navigation- reroute all 404 to index.html
           {
-            errorCachingMinTtl: Duration.days(365).toSeconds(),
-            errorCode: 404,
-            responseCode: 200,
+            ttl: Duration.days(365),
+            httpStatus: 404,
+            responseHttpStatus: 200,
             responsePagePath: this.getEnvVariable("NOT_FOUND_PATH"),
           },
           {
-            errorCachingMinTtl: Duration.days(365).toSeconds(),
-            errorCode: 403,
-            responseCode: 200,
+            ttl: Duration.days(365),
+            httpStatus: 403,
+            responseHttpStatus: 200,
             responsePagePath: this.getEnvVariable("NOT_FOUND_PATH"),
           },
         ],
-        originConfigs: [
-          {
-            s3OriginSource: {
-              s3BucketSource: websiteBucket,
-            },
-            behaviors: [
-              {
-                // index.html should not be cached based on name
-                defaultTtl: Duration.seconds(0),
-                maxTtl: Duration.days(0),
-                minTtl: Duration.days(0),
-                isDefaultBehavior: true,
-              },
-            ],
-          },
-        ],
-        viewerCertificate: {
-          aliases: [routingDomain],
-          props: {
-            acmCertificateArn: this.getSslCert(accountId),
-            sslSupportMethod: cloudfront.SSLMethod.SNI,
-          },
-        },
+        domainNames: [routingDomain],
+        certificate: acm.Certificate.fromCertificateArn(
+          this,
+          "Certificate",
+          this.getSslCert(accountId)
+        ),
       }
     );
 
